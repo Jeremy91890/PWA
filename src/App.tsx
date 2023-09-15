af102d87caa81d789ee3d6a6523c747d1595baf8
Jérémy DEBELLEIX <jeremy.debelleix@modiscloud.net>
2023-09-15 11:41:29 +0200

    init pwa

---
+++ src/App.tsx
@@ -0,0 +1,33 @@
+import "./App.css";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import About from "./pages/About";
+import Users from "./pages/Users";
+import Home from "./pages/Home";
+
+export default function App() {
+  return (
+    <BrowserRouter>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/about">About</Link>
+            </li>
+            <li>
+              <Link to="/users">Users</Link>
+            </li>
+          </ul>
+        </nav>
+
+        <Routes>
+          <Route path="/about" element={<About />} />
+          <Route path="/users" element={<Users />} />
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
+  );
+}
---
+++ vite.config.ts
@@ -0,0 +1,31 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { VitePWA, VitePWAOptions } from "vite-plugin-pwa";
+
+const manifestForPlugin: Partial<VitePWAOptions> = {
+  registerType: "prompt",
+  includeAssets: ["favicon.svg", "robots.txt", "apple-touch-icon.png"],
+  manifest: {
+    name: "My App",
+    short_name: "My App",
+    description: "My App is the best app in the world",
+    theme_color: "#ffffff",
+    icons: [
+      {
+        src: "/logo192.png",
+        sizes: "192x192",
+        type: "image/png",
+      },
+      {
+        src: "/logo512.png.png",
+        sizes: "512x512",
+        type: "image/png",
+      },
+    ],
+  },
+};
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react(), VitePWA(manifestForPlugin)],
+});
