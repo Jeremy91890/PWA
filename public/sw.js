1e9d9a44aea839f50306e603e38ad2fd754bb7b3
Jérémy DEBELLEIX <jeremy.debelleix@modiscloud.net>
2023-09-15 16:46:38 +0200

    try workbox

--- public/sw.js
+++ public/sw.js
@@ -22,6 +22,24 @@ self.addEventListener("install", (event) => {
   );
 });
 
+importScripts(
+  "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js"
+);
+workbox.setConfig({ debug: false });
+workbox.precaching.precacheAndRoute([]);
+
+workbox.routing.registerRoute(
+  /\.(?:png|jpg|jpeg|svg|gif)$/, //What image file types do you care about caching
+  new workbox.strategies.CacheFirst({
+    cacheName: "image-cache",
+    plugins: [
+      new workbox.expiration.Plugin({
+        maxEntries: 20,
+        maxAgeSeconds: 7 * 24 * 60 * 60,
+      }),
+    ],
+  })
+);
 // //FETCH ALL IN OFFLINE MODE
 // this.addEventListener('fetch', (event) => {
 
