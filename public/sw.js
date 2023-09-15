9816f073bf34536451a8d8967c6da84aa701c52e
Jérémy DEBELLEIX <jeremy.debelleix@modiscloud.net>
2023-09-15 17:56:45 +0200

    here we go again

--- public/sw.js
+++ public/sw.js
@@ -1,65 +1,97 @@
-console.log("Registered");
+// Version definition
+let CACHE_VERSION = "ca-v1";
+let DISP_VERSION = "ca-d-v1";
 
-//CHACHE ALL FILES
-let cacheData = "appv1";
-self.addEventListener("install", (event) => {
+// Directory to be cached (css / js is added individually)
+let resources = ["/", "/images"];
+
+// Add cache
+self.addEventListener("install", function (event) {
+  console.log("ServiceWorker Install");
   event.waitUntil(
-    caches.open(cacheData).then((cacheres) => {
-      cacheres.addAll([
-        "/favicon.ico",
-        "/index.html",
-        "/images/",
-        "/images/1.png",
-      ]);
+    caches.open(CACHE_VERSION).then(function (cache) {
+      console.log("cache.addAll");
+      cache.addAll(resources);
     })
   );
 });
+// Shiw cash
+self.addEventListener("fetch", function (event) {
+  console.log("ServiceWorker fetch");
+  event.respondWith(
+    // Check if cache exists
+    caches.match(event.request).then(function (response) {
+      if (response) {
+        return response;
+      } else {
+        // If there is no cache, put it in the cache
+        return fetch(event.request)
+          .then(function (res) {
+            return caches.open(DISP_VERSION).then(function (cache) {
+              console.log("cache.put");
+              cache.put(event.request.url, res.clone());
+              return res;
+            });
+          })
+          .catch(function () {
+            // do nothing
+          });
+      }
+    })
+  );
+});
+// Delete old cache
+self.addEventListener("activate", function (event) {
+  console.log("activate ServiceWorker");
+  event.waitUntil(
+    caches.keys().then(function (keyList) {
+      return Promise.all(
+        keyList.map(function (key) {
+          if (key !== CACHE_VERSION && key !== DISP_VERSION) {
+            console.log("cache.delete");
+            return caches.delete(key);
+          }
+        })
+      );
+    })
+  );
+  return self.clients.claim();
+});
 
-importScripts(
-  "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js"
-);
-workbox.setConfig({ debug: false });
-workbox.precaching.precacheAndRoute([]);
+// console.log("Registered");
 
-workbox.routing.registerRoute(
-  /\.(?:png|jpg|jpeg|svg|gif)$/, //What image file types do you care about caching
-  new workbox.strategies.CacheFirst({
-    cacheName: "image-cache",
-    plugins: [
-      new workbox.expiration.Plugin({
-        maxEntries: 20,
-        maxAgeSeconds: 7 * 24 * 60 * 60,
-      }),
-    ],
-  })
-);
-// //FETCH ALL IN OFFLINE MODE
-// this.addEventListener('fetch', (event) => {
+// //CHACHE ALL FILES
+// let cacheData = "appv1";
+// self.addEventListener("install", (event) => {
+//   event.waitUntil(
+//     caches.open(cacheData).then((cacheres) => {
+//       cacheres
+//         .addAll(["/favicon.ico", "/index.html"])
+//         .then(() => {
+//           console.log("Files are cached");
+//         })
+//         .catch((err) => {
+//           console.log("Error while caching files: ", err);
+//         });
+//     })
+//   );
+// });
 
-//     if (!navigator.onLine) {
+// importScripts(
+//   "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js"
+// );
+// workbox.setConfig({ debug: false });
+// workbox.precaching.precacheAndRoute([]);
 
-//         //PUSH NOTIFICATION WHEN URL IS BELOW MENTIONED ONE
-//         if (event.request.url === 'http://localhost:3000/manifest.json') {
-//             event.waitUntil(
-//                 this.registration.showNotification("You Are Offline", {
-//                     body: "Make sure that you internet connection is stable to get the latest updates and feature. Thankyou!"
-//                 })
-//             )
-//         }
-
-//         event.respondWith(
-//             caches.match(event.request).then((response) => {
-//                 if (response) {
-//                     return response
-//                 }
-//                 let requestUrl = event.request.clone()
-//                 fetch(requestUrl).then((res) => {
-//                     console.log(res)
-//                 }).catch((err) => {
-//                     console.log(err)
-//                 })
-
-//             })
-//         )
-//     }
-// })
+// workbox.routing.registerRoute(
+//   /\.(?:png|jpg|jpeg|svg|gif)$/, //What image file types do you care about caching
+//   new workbox.strategies.CacheFirst({
+//     cacheName: "image-cache",
+//     plugins: [
+//       new workbox.expiration.Plugin({
+//         maxEntries: 20,
+//         maxAgeSeconds: 7 * 24 * 60 * 60,
+//       }),
+//     ],
+//   })
+// );
--- src/main.tsx
+++ src/main.tsx
@@ -3,21 +3,48 @@ import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 
-if ("serviceWorker" in navigator) {
-  window.addEventListener("load", () => {
+// if ("serviceWorker" in navigator) {
+//   window.addEventListener("load", () => {
+//     navigator.serviceWorker
+//       .register("/sw.js")
+//       .then((registration) => {
+//         console.log(
+//           "Service Worker registered with scope:",
+//           registration.scope
+//         );
+//       })
+//       .catch((error) => {
+//         console.error("Service Worker registration failed:", error);
+//       });
+//   });
+// }
+
+window.addEventListener("load", function () {
+  if ("serviceWorker" in navigator) {
     navigator.serviceWorker
       .register("/sw.js")
-      .then((registration) => {
-        console.log(
-          "Service Worker registered with scope:",
-          registration.scope
-        );
+      .then(function (registration) {
+        return registration.pushManager
+          .getSubscription()
+          .then(function (subscription) {
+            console.log("subscription", subscription);
+            if (subscription) {
+              return subscription;
+            }
+            return registration.pushManager.subscribe({
+              userVisibleOnly: true,
+            });
+          });
       })
-      .catch((error) => {
-        console.error("Service Worker registration failed:", error);
+      .then(function (subscription) {
+        let endpoint = subscription.endpoint;
+        console.log("pushManager endpoint:", endpoint);
+      })
+      .catch(function (error) {
+        console.log("serviceWorker error:", error);
       });
-  });
-}
+  }
+});
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
